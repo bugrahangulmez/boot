b2ef818ebf57bdb48d6e1f95bbc6e3e91353459c
bugrahan gulmez <114365074+bugrahangulmez@users.noreply.github.com>
2023-12-09 23:30:27 +0300

    coach option added

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const {
   pazar1,
   pazar2,
   CORTS,
-} = require("./data");
+} = require("./data")
 
 const SEANS = {
   pazartesi1,
@@ -31,45 +31,68 @@ const SEANS = {
   cumartesi2,
   pazar1,
   pazar2,
-};
+}
 
-const { CourtReservation } = require("./functions");
+const { CourtReservation, CourtReservationCoach } = require("./functions")
 
-const bgratc = "45856276834";
-const bugrapass = "71936582";
+const bgratc = "45856276834"
+const bugrapass = "71936582"
 
-const yusuftc = "18821832878";
-const yusufpass = "Artisboy67";
+const yusuftc = "18821832878"
+const yusufpass = "Artisboy67"
 
-const tc = bgratc;
-const password = bugrapass;
+const tc = bgratc
+const password = bugrapass
 
 // !!! RANDEVU ALACAĞIN GÜNÜ KONTROL ETMEYİ UNUTMA !!!
 
-const seans = cuma1.btn10;
-const kort = CORTS.KAPALI_6;
+const seans = sali2.btn1
+const kort = CORTS.TOPRAK_2
 
 // !!! RANDEVU ALACAĞIN SAATİ KONTROL ET !!!
 
 // DAKİKA VE SANİYE "O" OLARAK KALSIN
 
-const hourInputVar = 20;
-const minuteInputVar = 0;
-const secondInputVar = 0;
+const hourInputVar = 22
+const minuteInputVar = 32
+const secondInputVar = 10
+
+// NORMAL KİRALAMA İÇİN "hiring" EĞİTMEN KİRALAMA İÇİN "coach" SEÇİLECEK
+
+const hiringTypes = {
+  coach: "Dışarıdan Eğitmen",
+  hiring: "Kort Kiralama",
+}
+
+const type = hiringTypes.coach
+
+const personCount = 2
 
 // !!! GÜNÜN YANINDAKİ "btn" KISMINDA DOĞRU btn'yi SEÇTİĞİNDEN EMİN OL !!!
 
 // DAHA SONRA "npm run dev" KOMUTU İLE BOTU ÇALIŞTIRABİLİRSİN
 
-CourtReservation(
+// CourtReservation(
+//   tc,
+//   password,
+//   seans,
+//   kort,
+//   hourInputVar,
+//   minuteInputVar,
+//   secondInputVar
+// );
+
+CourtReservationCoach({
   tc,
   password,
-  seans,
   kort,
-  hourInputVar,
-  minuteInputVar,
-  secondInputVar
-);
+  seans,
+  hourInput: hourInputVar,
+  minuteInput: minuteInputVar,
+  secondInput: secondInputVar,
+  personCount,
+  type,
+})
 
 // Fonksiyona parametre olarak verilecek değişkenlerin sırası;
 // 1 - Tc kimlik no (Kullanıcı adı) = tc
